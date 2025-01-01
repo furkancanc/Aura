d4355702680c4a1bf73ba9f565752248dd634987
furkancanc <fcc.ceylan@gmail.com>
2025-01-01 19:50:08 +0300

    Game Mode - Player Controller Update & PlayerCharacter Animation Fix

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,18 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	UCharacterMovementComponent* CharacterMovementComponent = GetCharacterMovement();
+	
+	CharacterMovementComponent->bOrientRotationToMovement = true;
+	CharacterMovementComponent->RotationRate = FRotator(0.f, 400.f, 0.f);
+	CharacterMovementComponent->bConstrainToPlane = true;
+	CharacterMovementComponent->bSnapToPlaneAtStart = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll	= false;
+	bUseControllerRotationYaw	= false;
+}
